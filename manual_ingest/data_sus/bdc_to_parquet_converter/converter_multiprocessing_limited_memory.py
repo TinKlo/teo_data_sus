9631b1721711ec1ed7465624dd5b618ccb78c713
chic <thiagoiklock@gmail.com>
2023-07-13 21:37:16 -0300

    folders organisation and convert from bdc to parquet whit multiprocessing and memory usage limit.

--- manual_ingest/data_sus/bdc_to_parquet_converter/converter_multiprocessing_limited_memory.py
+++ manual_ingest/data_sus/bdc_to_parquet_converter/converter_multiprocessing_limited_memory.py
@@ -2,7 +2,17 @@ import os
 import pandas as pd
 from dbc_reader import DbcReader
 from multiprocessing import Pool
-import resource
+from itertools import islice
+
+def process_chunk(chunk, file_path):
+    # Convert the chunk to a DataFrame
+    df = pd.DataFrame(chunk)
+    
+    # Save DataFrame to Parquet file
+    parquet_file_path = os.path.splitext(file_path)[0] + '.parquet'
+    df.to_parquet(parquet_file_path)
+    
+    print(f"Chunk processed and saved as '{parquet_file_path}'")
 
 def process_file(file_name):
     if file_name.endswith('.dbc'):
@@ -10,23 +20,17 @@ def process_file(file_name):
         file_path = os.path.join(folder_path, file_name)
         
         # Read the rows from the .dbc file using DbcReader
-        rows = [row for row in DbcReader(file_path)]
+        reader = DbcReader(file_path)
         
-        # Create a DataFrame from the rows
-        df = pd.DataFrame(rows)
+        # Define the chunk size
+        chunk_size = 10000  # Adjust the chunk size as per your memory constraints
         
-        # Save DataFrame to Parquet file
-        parquet_file_path = os.path.splitext(file_path)[0] + '.parquet'
-        df.to_parquet(parquet_file_path)
-        
-        print(f"File '{file_name}' converted to DataFrame and saved as '{parquet_file_path}'")
+        # Process the file in chunks
+        for chunk in iter(lambda: list(islice(reader, chunk_size)), []):
+            process_chunk(chunk, file_path)
 
 if __name__ == '__main__':
     folder_path = '/home/chic/repos/data_sus_tik/fct-unesp-datasus/landing/SIHSUS'
-    max_memory = int(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss * 0.85)  # 85% of the maximum RAM usage
-    
-    # Set the maximum memory limit for each worker process
-    resource.setrlimit(resource.RLIMIT_AS, (max_memory, max_memory))
     
     # Get the list of file names in the folder
     file_names = os.listdir(folder_path)
